544f0519b685e94bf7333a86b5fb9b570ba841fa
lucky <lakshay4dude@gmail.com>
2015-11-21 13:15:31 +0530

    working without searching and edition

--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -56,54 +56,40 @@ def register_page(request):
 			'registration/register.html',variables)
 
 
-@login_required
 def bookmark_save_page(request):
-  ajax=request.GET.has_key('ajax')
   if request.method == 'POST':
     form = BookmarkSaveForm(request.POST)
     if form.is_valid():
-      bookmark1 = _bookmark_save(form)
-      if ajax:
-      	variables=RequestContext(request,{'bookmarks':[bookmark1],
-      		'show_edit':True,
-      		'show_tags':True
-      		})
-      	return render_to_response('bookmark_list.html',variables)
-      else:
-      	return HttpResponseRedirect('/user/%s/' % request.user.username)
-    else:
-    	if ajax:
-    		return HttpResponse('failure')
-  elif request.GET.has_key('url'):
-    url = request.GET['url']
-    title = ''
-    tags = ''
-    try:
-      link = Link.objects.get(url=url)
-      bookmark1 =bookmark.objects.get(
-        link=link,
-        user=request.user
+# Create or get link.
+      link, dummy = Link.objects.get_or_create(
+        url=form.cleaned_data['url']
       )
-      title = bookmark1.title
-      tags = ' '.join(
-        tag.name for tag in bookmark1.tag_set.all()
+      # Create or get bookmark.
+      bookmark1, created = bookmark.objects.get_or_create(
+        user=request.user,
+        link=link
       )
-    except ObjectDoesNotExist:
-      pass
-    form = BookmarkSaveForm({
-      'url': url,
-      'title': title,
-      'tags': tags
-    })
+      # Update bookmark title.
+      bookmark1.title = form.cleaned_data['title']
+      # If the bookmark is being updated, clear old tag list.
+      if not created:
+        bookmark1.tag_set.clear()
+      # Create new tag list.
+      tag_names = form.cleaned_data['tags'].split()
+      for tag_name in tag_names:
+        tag, dummy = Tag.objects.get_or_create(name=tag_name)
+        bookmark1.tag_set.add(tag)
+      # Save bookmark to database.
+      bookmark1.save()
+      return HttpResponseRedirect(
+        '/user/%s/' % request.user.username
+      )
   else:
     form = BookmarkSaveForm()
   variables = RequestContext(request, {
     'form': form
   })
-  if ajax:
-  	return render_to_response('bookmark_save_form.html', variables)
-  else:
-  	return render_to_response('bookmark_save.html',variables)
+  return render_to_response('bookmark_save.html', variables)
 
 def tag_cloud_page(request):
  	MAX_WEIGHT=5
@@ -124,57 +110,3 @@ def tag_cloud_page(request):
  		'tags':tags
  		})
  	return render_to_response("tag_cloud_page.html",var)
-@login_required
-def search_page(request):
-  form = SearchForm()
-  bookmarks = []
-  show_results = False
-  if request.GET.has_key('query'):
-    show_results = True
-    query = request.GET['query'].strip()
-    if query:
-      form = SearchForm({'query' : query}) 
-      bookmarks = \
-        bookmark.objects.filter (title__icontains=query)[:10]
-  variables = RequestContext(request, { 
-  	'form': form,
-    'bookmarks': bookmarks,
-    'show_results': show_results,
-    'show_tags': True,
-    'show_user': True
-  })
-  if request.GET.has_key('ajax'):
-  	return render_to_response('bookmark_list.html',variables)
-  else:
-  	return render_to_response('search.html', variables)
-
-
-@login_required
-def _bookmark_save(request,form):
-  if request.method == 'POST':
-    form = BookmarkSaveForm(request.POST)
-    if form.is_valid():
-   
-      link, dummy = Link.objects.get_or_create(
-        url=form.cleaned_data['url']
-      )
-      
-      bookmark1, created = bookmark.objects.get_or_create(
-        user=request.user,
-        link=link
-      )
-      
-      bookmark1.title = form.cleaned_data['title']
-      
-      if not created:
-        bookmark1.tag_set.clear()
-
-      tag_names = form.cleaned_data['tags'].split()
-      for tag_name in tag_names:
-        tag, dummy = Tag.objects.get_or_create(name=tag_name)
-        bookmark1.tag_set.add(tag)
-    
-      bookmark1.save()
-      return bookmark1;
-
-
